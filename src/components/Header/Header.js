dda02d811d383a5cbc809a91f6dc8ac5aad7a815
AlissonZaramello <alissonzaramello34@gmail.com>
2025-06-11 22:27:35 -0300

    Criacao da estrutura da sidebar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,15 @@
 import styles from './header.module.css';
 import Logo from '../../assets/logo.png';
+import Sidebar from '../Sidebar/Sidebar'
+import { useState } from 'react';
 
 export default function Header() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () =>{
+        setIsSidebarOpen(!isSidebarOpen)
+    }
+
     return (
         <>
             <header className={styles.header}>
@@ -13,9 +21,10 @@ export default function Header() {
                             <img src={Logo} alt="Instituto Federal - Campus Birigui"/>
                         </div>    
 
-                        <button aria-label="Abrir menu">
+                        <button aria-label="Abrir menu" onClick={toggleSidebar}>
                             <svg width="44" height="34" viewBox="0 0 54 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 43.5V36.3333H54V43.5H0ZM0 25.5833V18.4167H54V25.5833H0ZM0 7.66667V0.5H54V7.66667H0Z" fill="white"/></svg>
-                        </button>        
+                        </button>
+                        <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar}/>        
 
                     </div>
                 </div>   
