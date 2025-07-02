ad88a08ca9b127a5c2717cbec63754a32c2ca397
Pedro-Eugelmi <pedrogabrielmendy@gmail.com>
2025-07-02 13:20:40 -0300

    ajuste

--- src/components/categoriesList/CategoriesList.js
+++ src/components/categoriesList/CategoriesList.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './categories.module.css';
 
 export default function CategoriesList({categories, handleRemove}) {
@@ -30,10 +31,7 @@ export default function CategoriesList({categories, handleRemove}) {
                                             <td>{category.name}</td>
                                             
                                             <td className={styles.actions}>
-                                                
-                                                <button className={styles.actionsDelete}>
-                                                    <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill="currentColor" viewBox="0 0 24 24" ><path d="m17.71 7.29-3-3a.996.996 0 0 0-1.41 0l-11.01 11A1 1 0 0 0 2 16v3c0 .55.45 1 1 1h3c.27 0 .52-.11.71-.29l11-11a.996.996 0 0 0 0-1.41ZM5.59 18H4v-1.59l7.5-7.5 1.59 1.59zm8.91-8.91L12.91 7.5 14 6.41 15.59 8zM11 18h11v2H11z"></path></svg>                                                
-                                                </button>
+                                    
                                                 
                                                 <button onClick={handleRemove} data-remove={category.id} className={styles.deleteButton}><svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill="currentColor" viewBox="0 0 24 24" ><path d="M17 6V4c0-1.1-.9-2-2-2H9c-1.1 0-2 .9-2 2v2H2v2h2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8h2V6zM9 4h6v2H9zM6 20V8h12v12z"></path><path d="M9 10h2v8H9zM13 10h2v8h-2z"></path></svg></button>
                                             
